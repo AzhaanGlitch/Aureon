90092cbd3537bd831cbcf2f927abcd0c308e31e0
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2026-01-25 22:34:35 +0530

    indian rupeee

--- src/pages/onboarding/LandingPage.jsx
+++ src/pages/onboarding/LandingPage.jsx
@@ -135,7 +135,7 @@ const DollarSign = ({ size = 24, className = '' }) => (
     className={className}
   >
     <line x1="12" y1="1" x2="12" y2="23"></line>
-    <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+    <path d="M6 3h12M6 8h12m-5.6 13L6 13h3c5 0 8-2 8-5H6"></path>
   </svg>
 );
 
